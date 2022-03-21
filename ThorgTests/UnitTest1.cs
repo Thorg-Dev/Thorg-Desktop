7c9c93940e6f97207f236849e7ecdf6cca334ad4
scx1332 <sieciech.czajka@golem.network>
2022-03-21 15:47:44 +0100

    Added unit tests

---
+++ ThorgTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ThorgTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
