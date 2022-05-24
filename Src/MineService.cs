0122acf7f8683d7672dcd09848319fed18a9b194
Przemyslaw Rekucki <przemyslaw.rekucki@golem.network>
2022-05-24 18:57:33 +0200

    wip

--- Src/MineService.cs
+++ Src/MineService.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GolemUI.Miners;
 
 namespace GolemUI.Src
 {
     public class MineService
     {
+        public async Task Start(IMinerApp minerApp, MinerAppConfiguration configuration)
+        {
 
+        }
     }
 }
